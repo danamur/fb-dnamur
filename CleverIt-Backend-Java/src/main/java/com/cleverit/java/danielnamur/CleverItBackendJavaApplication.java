cbe6a6dff4cdca8f657e8f20d0109c003b3a7d34
Gamb <d.namur.n@gmail.com>
2020-12-01 15:27:46 -0300

    First commit

---
+++ CleverIt-Backend-Java/src/main/java/com/cleverit/java/danielnamur/CleverItBackendJavaApplication.java
@@ -0,0 +1,13 @@
+package com.cleverit.java.danielnamur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleverItBackendJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleverItBackendJavaApplication.class, args);
+	}
+
+}
---
+++ CleverIt-Backend-Java/src/test/java/com/cleverit/java/danielnamur/CleverItBackendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleverit.java.danielnamur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleverItBackendJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
